a5be0ec71a2e56a2ca045db1a6244fca83d2349b
Megha Nagabhushan <meghanagabhushan@MacBook-Air-3.local>
2024-12-04 00:37:12 -0500

    day 3 solution 1

--- year_2024/utils.py
+++ year_2024/utils.py
@@ -3,3 +3,8 @@ def get_file_lines_as_list(filename):
         lines = file.readlines()
         lines = [line.rstrip() for line in lines]
     return lines
+
+def get_file_as_str(filename):
+    with open(filename) as file:
+        content = file.read()
+    return content
